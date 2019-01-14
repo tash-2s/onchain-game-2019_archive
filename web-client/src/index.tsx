f683d9f4f71a5e7136f6dbb6d9d1bb86c9fc3de6
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-01-14 12:40:42 +0900

    store.ts is not needed to separate from index.tsx
    ---
    
    Original commit (web-client): 9d2de2bf36c03d03a19c4e12b828ffa4f1145653

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -1,12 +1,17 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import { createStore } from "redux"
 import { Provider } from "react-redux"
 
-import { buildStore } from "./store"
+import { reducer } from "./reducers/reducer"
 import { registerStore } from "./utils/route"
 import { AppContainer } from "./containers/AppContainer"
 
-const store = buildStore()
+const store = createStore(
+  reducer,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+)
 registerStore(store)
 ReactDOM.render(
   <Provider store={store}>
--- web-client/src/store.ts
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux"
-import { reducer } from "./reducers/reducer"
-
-export const buildStore = () =>
-  createStore(
-    reducer,
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-  )
