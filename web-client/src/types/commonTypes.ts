1a16c1a39733ae6ceb6ec3f506cd84478bea83f7
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-01-15 18:10:55 +0900

    Define PlanetsData
    ---
    
    Original commit (web-client): 025a8d314f9d7fba4224e6b7b02acc7c8a5bb93e

--- web-client/src/types/commonTypes.ts
+++ web-client/src/types/commonTypes.ts
@@ -9,6 +9,22 @@ export interface RouteState {
   params: Array<string>
 }
 
+interface NormalNonMagicPlanet {
+  id: number
+  kind: "residence" | "goldvein" | "technology"
+  param: number
+}
+
+interface NormalMagicPlanet {
+  id: number
+  kind: "magic"
+  hogeParam: "todo"
+}
+
+export type NormalPlanet = NormalNonMagicPlanet | NormalMagicPlanet
+
+// type PlanetKind = NormalNonMagicPlanet['kind'] | NormalMagicPlanet['kind']
+
 // ref. https://stackoverflow.com/questions/44480644/typescript-string-union-to-string-array
 type Lit = string | number | boolean | undefined | null | void | {}
 const tuple = <T extends Lit[]>(...args: T) => args
