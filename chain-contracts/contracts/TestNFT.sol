ae07e4355d9f0c7c9306048cbc0977302abb0c1b
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-02-02 11:49:52 +0900

    yarn lint
    ---
    
    Original commit (chain-contracts): a04bec5234a4bc537802cbac69753f5e0158edf8

--- chain-contracts/contracts/TestNFT.sol
+++ chain-contracts/contracts/TestNFT.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.4.24;
 
-import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
-import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
 
 contract TestNFT is ERC721Full, ERC721Mintable {
   constructor() ERC721Full("TestNFT", "TNFT") public {
