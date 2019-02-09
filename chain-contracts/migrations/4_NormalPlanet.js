7468764f9487a0b43243326dc24eb58eea6d4bed
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-02-09 22:39:40 +0900

    Create dummy data for test
    ---
    
    Original commit (chain-contracts): bc645d20e2f2cc53f5211cd7e6b8b3b879ec2204

--- chain-contracts/migrations/4_NormalPlanet.js
+++ chain-contracts/migrations/4_NormalPlanet.js
@@ -2,7 +2,9 @@ const NormalPlanet = artifacts.require("./NormalPlanet.sol")
 const UserNormalPlanet = artifacts.require("./UserNormalPlanet.sol")
 
 module.exports = function(deployer, network) {
-  deployer.deploy(NormalPlanet).then(function(normalPlanet) {
-    return deployer.deploy(UserNormalPlanet, normalPlanet.address)
+  deployer.then(async function() {
+    const normalPlanet = await deployer.deploy(NormalPlanet)
+    const userNormalPlanet = await deployer.deploy(UserNormalPlanet, normalPlanet.address)
+    await normalPlanet.create(1,2,3,4)
   })
 }
