5101a49df39abe30d8ea31bf92ec811a116b745d
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-02-14 13:15:14 +0900

    Remove tmp codes
    ---
    
    Original commit (chain-contracts): b6c3ac18c2106586a9a6060c83348873f4d976e2

--- chain-contracts/contracts/TestNFT.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.4.24;
-
-import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
-import "openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
-
-contract TestNFT is ERC721Full, ERC721Mintable {
-  constructor() public ERC721Full("TestNFT", "TNFT") {}
-}
--- chain-contracts/migrations/2_test_nft.js
+++
@@ -1,5 +0,0 @@
-const TestNFT = artifacts.require("./TestNFT.sol")
-
-module.exports = function(deployer, network) {
-  deployer.deploy(TestNFT)
-}
