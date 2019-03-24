6994cbce5a104a971bdf0fa1a811aa645b31faf4
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-03-24 17:14:26 +0900

    Migrations is MinterRole
    This makes my development process convinient.
    
    ---
    
    Original commit (chain-contracts): b8529fc749a72a89d8f86a76d4661d667e7c2318

--- chain-contracts/contracts/Migrations.sol
+++ chain-contracts/contracts/Migrations.sol
@@ -1,22 +1,15 @@
-pragma solidity >=0.4.21<0.6.0;
+pragma solidity 0.4.24;
 
-contract Migrations {
-  address public owner;
+import "openzeppelin-solidity/contracts/access/roles/MinterRole.sol";
+
+contract Migrations is MinterRole {
   uint public last_completed_migration;
 
-  constructor() public {
-    owner = msg.sender;
-  }
-
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
-  function setCompleted(uint completed) public restricted {
+  function setCompleted(uint completed) public onlyMinter {
     last_completed_migration = completed;
   }
 
-  function upgrade(address new_address) public restricted {
+  function upgrade(address new_address) public onlyMinter {
     Migrations upgraded = Migrations(new_address);
     upgraded.setCompleted(last_completed_migration);
   }
