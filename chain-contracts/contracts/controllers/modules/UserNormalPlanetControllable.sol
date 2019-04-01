845b6df2382da8b19a4762ab7e4c271b62f7baed
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-04-01 23:55:00 +0900

    Refactor UserNormalPlanetControllable
    just changed namings, and lines of fns.
    and minor improvements.
    
    ---
    
    Original commit (chain-contracts): c3c36e6a058511c21bebd6cdfb81ad90132fccce

--- chain-contracts/contracts/controllers/modules/UserNormalPlanetControllable.sol
+++ chain-contracts/contracts/controllers/modules/UserNormalPlanetControllable.sol
@@ -37,6 +37,8 @@ contract UserNormalPlanetControllable is PermanenceInterpretable, TimeGettable {
   uint8 constant USER_NORMAL_PLANET_PERMANENCE_AXIAL_COORDINATE_R_START_DIGIT = 47;
   uint8 constant USER_NORMAL_PLANET_PERMANENCE_AXIAL_COORDINATE_R_END_DIGIT = 51;
 
+  uint256 constant USER_NORMAL_PLANET_PERMANENCE_ELEMENT_PLACEHOLDER = 10000000000000000000000000000000000000000000000000000000000000000000000000000; // solium-disable-line max-len
+
   UserNormalPlanetPermanence private _userNormalPlanetPermanence;
   UserNormalPlanetIdCounterPermanence private _userNormalPlanetIdCounterPermanence;
 
@@ -69,7 +71,7 @@ contract UserNormalPlanetControllable is PermanenceInterpretable, TimeGettable {
     UserNormalPlanetRecord[] memory records = new UserNormalPlanetRecord[](us.length);
 
     for (uint16 i = 0; i < us.length; i++) {
-      records[i] = buildUserNormalPlanetRecord(us[i]);
+      records[i] = buildUserNormalPlanetRecordFromUint256(us[i]);
     }
 
     return records;
@@ -85,7 +87,7 @@ contract UserNormalPlanetControllable is PermanenceInterpretable, TimeGettable {
     returns (UserNormalPlanetRecord)
   {
     uint256[] memory us = _userNormalPlanetPermanence.read(account);
-    uint256 target;
+    uint256 target = 0;
     uint16 tmpId;
 
     for (uint16 i = 0; i < us.length; i++) {
@@ -103,10 +105,86 @@ contract UserNormalPlanetControllable is PermanenceInterpretable, TimeGettable {
       }
     }
 
-    return buildUserNormalPlanetRecord(target);
+    if (target == 0) {
+      revert("user normal planet is not found");
+    }
+
+    return buildUserNormalPlanetRecordFromUint256(target);
   }
 
-  function buildUserNormalPlanetRecord(uint256 source)
+  function mintUserNormalPlanet(
+    address account,
+    uint16 normalPlanetId,
+    uint8 kind,
+    uint16 param,
+    int16 axialCoordinateQ,
+    int16 axialCoordinateR
+  ) internal {
+    UserNormalPlanetRecord[] memory records = userNormalPlanetRecordsOf(account);
+
+    for (uint16 i = 0; i < records.length; i++) {
+      // TODO: for big planets
+      if ((records[i].axialCoordinateQ == axialCoordinateQ) && (records[i].axialCoordinateR == axialCoordinateR)) {
+        revert("duplicated coordinates");
+      }
+    }
+
+    uint16 counter = _userNormalPlanetIdCounterPermanence.read(account);
+    _userNormalPlanetIdCounterPermanence.update(account, counter + 1);
+    _userNormalPlanetPermanence.pushElement(
+      account,
+      buildUint256FromUserNormalPlanetRecord(
+        UserNormalPlanetRecord(
+          counter,
+          normalPlanetId,
+          kind,
+          param,
+          1,
+          uint32now(),
+          uint32now(),
+          axialCoordinateQ,
+          axialCoordinateR
+        )
+      )
+    );
+  }
+
+  // TODO: unmint
+  // https://ethereum.stackexchange.com/questions/1527/how-to-delete-an-element-at-a-certain-index-in-an-array
+
+  function rankupUserNormalPlanet(address account, uint16 userPlanetId) internal {
+    UserNormalPlanetRecord[] memory records = userNormalPlanetRecordsOf(account);
+    UserNormalPlanetRecord memory record;
+
+    for (uint16 i = 0; i < records.length; i++) {
+      record = records[i];
+
+      if (record.id == userPlanetId) {
+        uint8 newRank = record.rank + 1;
+        require(newRank <= 30, "max rank");
+        _userNormalPlanetPermanence.updateByIndex(
+          account,
+          i,
+          buildUint256FromUserNormalPlanetRecord(
+            UserNormalPlanetRecord(
+              record.id,
+              record.normalPlanetId,
+              record.kind,
+              record.originalParam,
+              newRank,
+              uint32now(),
+              record.createdAt,
+              record.axialCoordinateQ,
+              record.axialCoordinateR
+            )
+          )
+        );
+        break;
+      }
+    }
+  }
+
+  function buildUserNormalPlanetRecordFromUint256(uint256 source)
     internal
     pure
     returns (UserNormalPlanetRecord)
@@ -166,7 +244,7 @@ contract UserNormalPlanetControllable is PermanenceInterpretable, TimeGettable {
         USER_NORMAL_PLANET_PERMANENCE_AXIAL_COORDINATE_Q_START_DIGIT,
         USER_NORMAL_PLANET_PERMANENCE_AXIAL_COORDINATE_Q_END_DIGIT
       )
-    ); // works well?
+    );
     int16 axialCoordinateR = int16(
       interpretPermanenceUint256(
         source,
@@ -176,7 +254,7 @@ contract UserNormalPlanetControllable is PermanenceInterpretable, TimeGettable {
     );
 
     if (kind == 0) {
-      revert("faild to build user normal planet, it's not defined");
+      revert("faild to build user normal planet, it's not created");
     }
 
     return UserNormalPlanetRecord(
@@ -192,138 +270,64 @@ contract UserNormalPlanetControllable is PermanenceInterpretable, TimeGettable {
     );
   }
 
-  function mintUserNormalPlanet(
-    address account,
-    uint16 normalPlanetId,
-    uint8 kind,
-    uint16 param,
-    int16 axialCoordinateQ,
-    int16 axialCoordinateR
-  ) internal {
-    UserNormalPlanetRecord[] memory records = userNormalPlanetRecordsOf(account);
-
-    for (uint16 i = 0; i < records.length; i++) {
-      // TODO: for big planets
-      if ((records[i].axialCoordinateQ == axialCoordinateQ) && (records[i].axialCoordinateR == axialCoordinateR)) {
-        revert("duplicated coordinates");
-      }
-    }
-
-    uint16 counter = _userNormalPlanetIdCounterPermanence.read(account);
-    _userNormalPlanetIdCounterPermanence.update(account, counter + 1);
-    _userNormalPlanetPermanence.pushElement(
-      account,
-      transformUserNormalPlanetAttributesToUint256(
-        counter,
-        normalPlanetId,
-        kind,
-        param,
-        1,
-        uint32now(),
-        uint32now(),
-        axialCoordinateQ,
-        axialCoordinateR
-      )
-    );
-  }
-
-  // TODO: unmint
-  // https://ethereum.stackexchange.com/questions/1527/how-to-delete-an-element-at-a-certain-index-in-an-array
-
-  function transformUserNormalPlanetAttributesToUint256(
-    uint16 id,
-    uint16 normalPlanetId,
-    uint8 kind,
-    uint16 param,
-    uint8 rank,
-    uint32 rankupedAt,
-    uint32 createdAt,
-    int16 axialCoordinateQ,
-    int16 axialCoordinateR
-  ) public pure returns (uint256) {
+  function buildUint256FromUserNormalPlanetRecord(UserNormalPlanetRecord record)
+    internal
+    pure
+    returns (uint256)
+  {
     uint256 n = reinterpretPermanenceUint256(
-      10000000000000000000000000000000000000000000000000000000000000000000000000000,
+      USER_NORMAL_PLANET_PERMANENCE_ELEMENT_PLACEHOLDER,
       USER_NORMAL_PLANET_PERMANENCE_ID_START_DIGIT,
       USER_NORMAL_PLANET_PERMANENCE_ID_END_DIGIT,
-      id
+      record.id
     );
     n = reinterpretPermanenceUint256(
       n,
       USER_NORMAL_PLANET_PERMANENCE_NORMAL_PLANET_ID_START_DIGIT,
       USER_NORMAL_PLANET_PERMANENCE_NORMAL_PLANET_ID_END_DIGIT,
-      normalPlanetId
+      record.normalPlanetId
     );
     n = reinterpretPermanenceUint256(
       n,
       USER_NORMAL_PLANET_PERMANENCE_KIND_START_DIGIT,
       USER_NORMAL_PLANET_PERMANENCE_KIND_END_DIGIT,
-      kind
+      record.kind
     );
     n = reinterpretPermanenceUint256(
       n,
       USER_NORMAL_PLANET_PERMANENCE_ORIGINAL_PARAM_START_DIGIT,
       USER_NORMAL_PLANET_PERMANENCE_ORIGINAL_PARAM_END_DIGIT,
-      param
+      record.originalParam
     );
     n = reinterpretPermanenceUint256(
       n,
       USER_NORMAL_PLANET_PERMANENCE_RANK_START_DIGIT,
       USER_NORMAL_PLANET_PERMANENCE_RANK_END_DIGIT,
-      rank
+      record.rank
     );
     n = reinterpretPermanenceUint256(
       n,
       USER_NORMAL_PLANET_PERMANENCE_RANKUPED_AT_START_DIGIT,
       USER_NORMAL_PLANET_PERMANENCE_RANKUPED_AT_END_DIGIT,
-      rankupedAt
+      record.rankupedAt
     );
     n = reinterpretPermanenceUint256(
       n,
       USER_NORMAL_PLANET_PERMANENCE_CREATED_AT_START_DIGIT,
       USER_NORMAL_PLANET_PERMANENCE_CREATED_AT_END_DIGIT,
-      createdAt
+      record.createdAt
     );
     n = reinterpretPermanenceUint256(
       n,
       USER_NORMAL_PLANET_PERMANENCE_AXIAL_COORDINATE_Q_START_DIGIT,
       USER_NORMAL_PLANET_PERMANENCE_AXIAL_COORDINATE_Q_END_DIGIT,
-      uint16(axialCoordinateQ)
+      uint16(record.axialCoordinateQ)
     );
     return reinterpretPermanenceUint256(
       n,
       USER_NORMAL_PLANET_PERMANENCE_AXIAL_COORDINATE_R_START_DIGIT,
       USER_NORMAL_PLANET_PERMANENCE_AXIAL_COORDINATE_R_END_DIGIT,
-      uint16(axialCoordinateR)
+      uint16(record.axialCoordinateR)
     );
-  }
-
-  function rankupUserNormalPlanet(address account, uint16 userPlanetId) internal {
-    UserNormalPlanetRecord[] memory records = userNormalPlanetRecordsOf(account);
-    UserNormalPlanetRecord memory record;
-
-    for (uint16 i = 0; i < records.length; i++) {
-      record = records[i];
-
-      if (record.id == userPlanetId) {
-        uint8 newRank = record.rank + 1;
-        require(newRank <= 30, "max rank");
-        _userNormalPlanetPermanence.updateByIndex(
-          account,
-          i,
-          transformUserNormalPlanetAttributesToUint256(
-            record.id,
-            record.normalPlanetId,
-            record.kind,
-            record.originalParam,
-            newRank,
-            uint32now(),
-            record.createdAt,
-            record.axialCoordinateQ,
-            record.axialCoordinateR
-          )
-        );
-        break;
-      }
-    }
   }
 }
