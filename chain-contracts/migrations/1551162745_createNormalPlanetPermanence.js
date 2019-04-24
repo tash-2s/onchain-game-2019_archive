628963744a2b719454dc868f169e23c7b1c8369b
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-04-24 23:07:47 +0900

    Update data
    ---
    
    Original commit (chain-contracts): 5a11ed087502d3a063fa62c7cd67bfd5b28c4219

--- chain-contracts/migrations/1551162745_createNormalPlanetPermanence.js
+++ chain-contracts/migrations/1551162745_createNormalPlanetPermanence.js
@@ -8,7 +8,11 @@ module.exports = function(deployer, network) {
 
     const data = [
       { id: 1, kind: "residence", param: 1, priceGold: 3 },
-      { id: 2, kind: "goldvein", param: 1, priceGold: 3 }
+      { id: 2, kind: "goldvein", param: 1, priceGold: 3 },
+      { id: 3, kind: "residence", param: 2, priceGold: 6 },
+      { id: 4, kind: "goldvein", param: 2, priceGold: 6 },
+      { id: 5, kind: "residence", param: 3, priceGold: 9 },
+      { id: 6, kind: "goldvein", param: 3, priceGold: 9 }
     ]
 
     for (i = 0; i < data.length; i++) {
