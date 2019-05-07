622dd70a68bb5b6c775db357bb4d3cd1f359b8f9
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-05-07 14:20:36 +0900

    Update data
    ---
    
    Original commit (web-client): 4bf71f87760141d01e086d7b8e2022927a7354c7

--- web-client/src/data/planets.ts
+++ web-client/src/data/planets.ts
@@ -3,17 +3,17 @@ import { NormalPlanet } from "../types/commonTypes"
 let id = 1
 let param = 1
 const residenceAndGoldvein: NormalPlanet[] = []
-while (id <= 32) {
+while (id <= 16) {
   residenceAndGoldvein.push(
     { id: id++, kind: "residence", param: param, priceGold: param * 3 },
     { id: id++, kind: "goldvein", param: param, priceGold: param * 3 }
   )
-  param++
+  param += 2
 }
 
 export const NormalPlanetsData: NormalPlanet[] = residenceAndGoldvein.concat([
   { id: 101, kind: "technology", param: 2, priceGold: 8 },
-  { id: 102, kind: "technology", param: 3, priceGold: 11 }
+  { id: 102, kind: "technology", param: 3, priceGold: 12 }
 ])
 
 export const getNormalPlanet = (id: number) => {
