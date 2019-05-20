7fc4cff0eee476ee5d8d7c2e9045bda9e4e55068
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-05-20 22:27:33 +0900

    Add BN formatter
    ---
    
    Original commit (web-client): bd86196e66d3b10682a74be7275d96100fca8519

---
+++ web-client/src/models/BNFormatter.ts
@@ -0,0 +1,38 @@
+import BN from "bn.js"
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+
+export class BNFormatter {
+  static pretty = (bn: BN): [string, string] => {
+    const separated = (
+      bn
+        .toString()
+        .split("")
+        .reverse()
+        .join("")
+        .match(/\d{1,3}/g) || ["0"]
+    )
+      .reverse()
+      .map(s =>
+        s
+          .split("")
+          .reverse()
+          .join("")
+      )
+
+    const alphabetIndex = separated.length - 1
+    if (alphabetIndex >= 26) {
+      throw new Error("out of range")
+    }
+    const symbol = alphabet[alphabetIndex]
+
+    let num: string
+    if (separated.length === 1) {
+      num = `${separated[0]}.000`
+    } else {
+      num = `${separated[0]}.${separated[1]}`
+    }
+
+    return [num, symbol]
+  }
+}
