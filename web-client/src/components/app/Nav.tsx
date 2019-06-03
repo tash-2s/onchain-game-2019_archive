c43ac1a1aa6ca07e521593ef22ce4bb2c7fa1715
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-06-03 17:11:15 +0900

    Use func style for Nav
    ---
    
    Original commit (web-client): 73514ac892f901246d594b42598a232ea85a3b61

--- web-client/src/components/app/Nav.tsx
+++ web-client/src/components/app/Nav.tsx
@@ -3,76 +3,79 @@ import * as React from "react"
 import { CommonState } from "../../types/commonTypes"
 import { InternalLink } from "../utils/InternalLink"
 
-export class Nav extends React.Component<{
+interface Props {
   currentUser: CommonState["currentUser"]
   signup: () => void
-}> {
-  render = () => {
-    // when an error occurs, this should be un-clickable, because the store will continue to have the error state
+}
+
+export function Nav(props: Props) {
+  // when an error occurs, this should be un-clickable, because the store will continue to have the error state
+  return (
+    <nav className={"navbar is-light"}>
+      <div className={"container"}>
+        <div className={"navbar-brand"}>
+          <InternalLink className={"navbar-item"} to={"/"}>
+            <strong>k2</strong>
+          </InternalLink>
+
+          <a className={"navbar-burger"}>
+            <span />
+            <span />
+            <span />
+          </a>
+        </div>
+
+        <div className={"navbar-menu"}>
+          <div className={"navbar-start"}>
+            <InternalLink className={"navbar-item"} to={"/users"}>
+              Remarkable Users
+            </InternalLink>
+
+            <InternalLink className={"navbar-item"} to={"/about"}>
+              About
+            </InternalLink>
+          </div>
+
+          <div className={"navbar-end"}>
+            <NavEndContent {...props} />
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+function NavEndContent(props: Props) {
+  if (props.currentUser) {
+    const user = props.currentUser
     return (
-      <nav className={"navbar is-light"}>
-        <div className={"container"}>
-          <div className={"navbar-brand"}>
-            <InternalLink className={"navbar-item"} to={"/"}>
-              <strong>k2</strong>
-            </InternalLink>
-
-            <a className={"navbar-burger"}>
-              <span />
-              <span />
-              <span />
-            </a>
-          </div>
-
-          <div className={"navbar-menu"}>
-            <div className={"navbar-start"}>
-              <InternalLink className={"navbar-item"} to={"/users"}>
-                Remarkable Users
-              </InternalLink>
-              <InternalLink className={"navbar-item"} to={"/about"}>
-                About
-              </InternalLink>
-            </div>
-
-            <div className={"navbar-end"}>{this.user()}</div>
-          </div>
+      <div className={"navbar-item has-dropdown is-hoverable"}>
+        <a className={"navbar-link"}>{user.address}</a>
+        <div className={"navbar-dropdown"}>
+          <InternalLink className={"navbar-item"} to={["/:address", { address: user.address }]}>
+            My Page
+          </InternalLink>
+          <a className={"navbar-item"}>Log out (TODO)</a>
         </div>
-      </nav>
+      </div>
     )
-  }
+  } else {
+    return (
+      <div className={"navbar-item"}>
+        <div className={"field is-grouped"}>
+          <p className={"control"}>
+            <button className={"button is-primary"} onClick={props.signup}>
+              Sign up
+            </button>
+          </p>
 
-  user = () => {
-    if (this.props.currentUser) {
-      const user = this.props.currentUser
-      return (
-        <div className={"navbar-item has-dropdown is-hoverable"}>
-          <a className={"navbar-link"}>{user.address}</a>
-          <div className={"navbar-dropdown"}>
-            <InternalLink className={"navbar-item"} to={["/:address", { address: user.address }]}>
-              My Page
-            </InternalLink>
-            <a className={"navbar-item"}>Log out (TODO)</a>
-          </div>
+          <p className={"control"}>
+            <button className={"button is-link"} disabled={true}>
+              Log in (TODO)
+            </button>
+          </p>
         </div>
-      )
-    } else {
-      return (
-        <div className={"navbar-item"}>
-          <div className={"field is-grouped"}>
-            <p className={"control"}>
-              <button className={"button is-primary"} onClick={this.props.signup}>
-                Sign up
-              </button>
-            </p>
-
-            <p className={"control"}>
-              <button className={"button is-link"} disabled={true}>
-                Log in (TODO)
-              </button>
-            </p>
-          </div>
-        </div>
-      )
-    }
+      </div>
+    )
   }
 }
