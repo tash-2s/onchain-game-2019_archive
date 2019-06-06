3905c72882672b57cdcc1cea5050b121db61b62d
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-06-06 11:01:24 +0900

    Add comments to clarify
    ---
    
    Original commit (web-client): a30b5af45ce4a1a669d87ac6bd6a25fb4e5aa179

--- web-client/src/components/routed/User/UserPlanetMap.tsx
+++ web-client/src/components/routed/User/UserPlanetMap.tsx
@@ -39,6 +39,10 @@ export class UserPlanetMap extends React.Component<Props, State> {
     }
 
     const shownRadius = this.props.user.map.shownRadius
+    // Show biggest map for this area
+    // Calc hex sizes to fit mapWidth
+    // mapWidth (= state.width) = (1.5 * hexSize) * (mapRadius * 2 + 1) + (0.5 * hexSize)
+    // Transform this equation for hexSize...
     const hexSize = this.state.width / (3 * shownRadius + 2)
     const hexWidth = hexSize * 2
     const hexHeight = Math.sqrt(3) * hexSize
