c58422e59d78c4306c1897a0536a2760f9382c2a
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-06-10 13:55:16 +0900

    Remove unused a Button component
    ---
    
    Original commit (web-client): 1e839ca3a158fa1a9a01911d772a8cf61a3dad79

--- web-client/src/components/utils/InternalLinkButton.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import { InternalLink } from "./InternalLink"
-
-export class InternalLinkButton extends InternalLink {
-  render = () => {
-    // in the future, `<a class="button">Button</a>` will be used (because it can have a href)
-    return <button onClick={this.go}>{this.props.children}</button>
-  }
-}
