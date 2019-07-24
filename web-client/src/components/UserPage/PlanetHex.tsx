56fd99e383f8b499f0a63be0e89c3e5e7514047c
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-07-24 22:15:16 +0900

    Setup canvas
    ---
    
    Original commit (web-client): 3a26af842539babb716c32ca405b3edf2a6366ae

--- web-client/src/components/UserPage/PlanetHex.tsx
+++ web-client/src/components/UserPage/PlanetHex.tsx
@@ -17,10 +17,21 @@ export function PlanetHex(props: {
   const x = props.hexSize * ((3 / 2) * props.q)
   const y = props.hexSize * ((Math.sqrt(3) / 2) * props.q + Math.sqrt(3) * props.r)
 
-  const buttonIfAvailable = !!props.setPlanet ? (
-    <button className={"button is-small"} onClick={props.setPlanet(props.q, props.r)}>
+  const cover = !!props.setPlanet ? (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        backgroundColor: "hsla(180deg, 100%, 50%, 50%)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+      onClick={props.setPlanet(props.q, props.r)}
+    >
       {props.userPlanet ? "replace" : "set"}
-    </button>
+    </div>
   ) : (
     <></>
   )
@@ -30,7 +41,7 @@ export function PlanetHex(props: {
     top: y + props.shiftTop,
     width: props.hexWidth,
     height: props.hexHeight,
-    backgroundColor: props.userPlanet ? "cyan" : "red",
+    backgroundColor: "grey",
     cursor: props.userPlanet ? "pointer" : "auto",
     clipPath: "polygon(75% 0, 100% 50%, 75% 100%, 25% 100%, 0 50%, 25% 0)",
     display: "flex",
@@ -39,9 +50,25 @@ export function PlanetHex(props: {
     position: "absolute"
   }
 
+  const canvasRef = React.useRef<HTMLCanvasElement>(null)
+  React.useEffect(() => {
+    const canvas = canvasRef.current
+    if (!canvas || !props.userPlanet) {
+      return
+    }
+    const ctx = canvas.getContext("2d")
+    if (!ctx) {
+      throw new Error("couldn't get a context from the canvas")
+    }
+
+    ctx.fillStyle = "#000000"
+    ctx.fillRect(0, 0, 100, 100)
+  })
+
   return (
     <div style={css} onClick={props.select}>
-      {buttonIfAvailable}
+      {props.userPlanet ? <canvas ref={canvasRef} /> : ""}
+      {cover}
     </div>
   )
 }
