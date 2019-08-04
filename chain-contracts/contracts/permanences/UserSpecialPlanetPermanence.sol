e658f4d2b20e046c64bd5700e8435495dd597d0d
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-08-04 14:50:22 +0900

    add UserSpecialPlanetPermanence
    ---
    
    Original commit (chain-contracts): 971300c907fd2b4f4c5c4c64091fff48b700a5f2

---
+++ chain-contracts/contracts/permanences/UserSpecialPlanetPermanence.sol
@@ -0,0 +1,6 @@
+pragma solidity 0.4.24;
+
+import "./abstracts/AddressToBytes32ArrayPermanence.sol";
+
+/* solium-disable no-empty-blocks */
+contract UserSpecialPlanetPermanence is AddressToBytes32ArrayPermanence {}
