a1c5d0f5122c81a8647a105293deb9eb528feeaf
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-08-09 15:35:32 +0900

    Add special planet's getter
    ---
    
    Original commit (chain-contracts): 2317e7aeb676a8e2a3b4db217f3a52bbce6715e3

--- chain-contracts/contracts/controllers/SpecialPlanetController.sol
+++ chain-contracts/contracts/controllers/SpecialPlanetController.sol
@@ -29,6 +29,43 @@ contract SpecialPlanetController is UserPlanetControllable {
     return _remarkableUserController;
   }
 
+  function planets(address account)
+    external
+    view
+    returns (
+    uint24[], // id
+    uint8[], // kind
+    uint8[], // originalParamCommonLogarithm
+    uint32[], // [rankupedAt, createdAt, ...]
+    int16[] // [q, r, ...]
+  )
+  {
+    UserSpecialPlanetRecord[] memory userPlanetRecords = userSpecialPlanetRecordsOf(account);
+    uint16 userPlanetsCount = uint16(userPlanetRecords.length);
+
+    uint24[] memory ids = new uint24[](userPlanetsCount);
+    uint8[] memory kinds = new uint8[](userPlanetsCount);
+    uint8[] memory params = new uint8[](userPlanetsCount);
+    uint32[] memory times = new uint32[](userPlanetsCount * 2);
+    int16[] memory coordinates = new int16[](userPlanetsCount * 2);
+
+    uint16 counter = 0;
+
+    for (uint16 i = 0; i < userPlanetsCount; i++) {
+      ids[i] = userPlanetRecords[i].id;
+      kinds[i] = userPlanetRecords[i].kind;
+      params[i] = userPlanetRecords[i].originalParamCommonLogarithm;
+      times[counter] = userPlanetRecords[i].rankupedAt;
+      times[counter + 1] = userPlanetRecords[i].createdAt;
+      coordinates[counter] = userPlanetRecords[i].axialCoordinateQ;
+      coordinates[counter + 1] = userPlanetRecords[i].axialCoordinateR;
+
+      counter += 2;
+    }
+
+    return (ids, kinds, params, times, coordinates);
+  }
+
   function getPlanet() external {
     // TODO: cost
     uint8 kind = 1; // TODO
