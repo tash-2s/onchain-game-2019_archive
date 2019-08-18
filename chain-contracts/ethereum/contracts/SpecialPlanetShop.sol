8159c4d851bab127f70c391b3060b03de76e17c7
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-08-18 15:34:36 +0900

    Real id token, except for random parts
    ---
    
    Original commit (chain-contracts): 1e8d348d6a17c8cdfc0eb061dc78a45034bac52b

--- chain-contracts/ethereum/contracts/SpecialPlanetShop.sol
+++ chain-contracts/ethereum/contracts/SpecialPlanetShop.sol
@@ -3,21 +3,39 @@ pragma solidity 0.4.24;
 import "./SpecialPlanet.sol";
 
 contract SpecialPlanetShop {
-  SpecialPlanet private _specialPlanet;
-  uint256 public tmpIdGenerator;
+  uint24 constant UINT24_MAX = ~uint24(0);
+
+  uint8 constant ID_SHORT_ID_START_BIT = 0;
+  uint8 constant ID_VERSION_START_BIT = 24;
+  uint8 constant ID_KIND_START_BIT = 24 + 8;
+  uint8 constant ID_OPCL_START_BIT = 24 + 8 + 8;
+  uint8 constant ID_ART_SEED_START_BIT = 24 + 8 + 8 + 8;
+
+  SpecialPlanet public specialPlanet;
+  uint256 public shortIdGenerator;
 
   constructor(address specialPlanetAddress) public {
-    _specialPlanet = SpecialPlanet(specialPlanetAddress);
-  }
-
-  function specialPlanet() external view returns (SpecialPlanet) {
-    return _specialPlanet;
+    specialPlanet = SpecialPlanet(specialPlanetAddress);
   }
 
   // TODO: get eth
   function sell() external returns (uint256) {
-    tmpIdGenerator++; // TODO: correct id
-    _specialPlanet.mint(msg.sender, tmpIdGenerator);
-    return tmpIdGenerator;
+    shortIdGenerator++;
+    require(shortIdGenerator <= UINT24_MAX, "short id is too big");
+
+    uint8 version = 1;
+    uint8 kind = 123; // todo
+    uint8 originalParamCommonLogarithm = 40;
+    uint64 artSeed = 456; // todo
+
+    uint256 id =
+      (uint256(shortIdGenerator) << ID_SHORT_ID_START_BIT) |
+      (uint256(version) << ID_VERSION_START_BIT) |
+      (uint256(kind) << ID_KIND_START_BIT) |
+      (uint256(originalParamCommonLogarithm) << ID_OPCL_START_BIT) |
+      (uint256(artSeed) << ID_ART_SEED_START_BIT);
+
+    specialPlanet.mint(msg.sender, id);
+    return id;
   }
 }
