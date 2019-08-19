14be27b465667fa45f7139f89af640247ecb3ae6
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-08-19 22:25:39 +0900

    Add random, and use it
    ---
    
    Original commit (chain-contracts): 7481d13ee46ff3f0b82384c6bd5ad14e8cb20bd0

--- chain-contracts/ethereum/contracts/SpecialPlanetShop.sol
+++ chain-contracts/ethereum/contracts/SpecialPlanetShop.sol
@@ -5,14 +5,15 @@ import "./SpecialPlanet.sol";
 contract SpecialPlanetShop {
   uint24 constant UINT24_MAX = ~uint24(0);
 
-  uint8 constant ID_SHORT_ID_START_BIT = 0;
-  uint8 constant ID_VERSION_START_BIT = 24;
-  uint8 constant ID_KIND_START_BIT = 24 + 8;
-  uint8 constant ID_OPCL_START_BIT = 24 + 8 + 8;
-  uint8 constant ID_ART_SEED_START_BIT = 24 + 8 + 8 + 8;
+  uint8 constant ID_VERSION_START_BIT = 0;
+  uint8 constant ID_SHORT_ID_START_BIT = 8;
+  uint8 constant ID_KIND_START_BIT = 8 + 24;
+  uint8 constant ID_OPCL_START_BIT = 8 + 24 + 8;
+  uint8 constant ID_ART_SEED_START_BIT = 8 + 24 + 8 + 8;
 
   SpecialPlanet public specialPlanet;
   uint256 public shortIdGenerator;
+  bytes32 private _s;
 
   constructor(address specialPlanetAddress) public {
     specialPlanet = SpecialPlanet(specialPlanetAddress);
@@ -23,19 +24,27 @@ contract SpecialPlanetShop {
     shortIdGenerator++;
     require(shortIdGenerator <= UINT24_MAX, "short id is too big");
 
+    bytes32 seed = _nextUnsafeSeed();
+
     uint8 version = 1;
-    uint8 kind = 123; // todo
+    uint8 kind = (uint8(seed) % 2) + 1; // 1 or 2
     uint8 originalParamCommonLogarithm = 40;
-    uint64 artSeed = 456; // todo
+    uint64 artSeed = uint64(seed >> 8);
 
     uint256 id =
-      (uint256(shortIdGenerator) << ID_SHORT_ID_START_BIT) |
       (uint256(version) << ID_VERSION_START_BIT) |
+      (uint256(shortIdGenerator) << ID_SHORT_ID_START_BIT) |
       (uint256(kind) << ID_KIND_START_BIT) |
       (uint256(originalParamCommonLogarithm) << ID_OPCL_START_BIT) |
       (uint256(artSeed) << ID_ART_SEED_START_BIT);
 
     specialPlanet.mint(msg.sender, id);
     return id;
+  }
+
+  // this is not secure, but enough for my use case, for now.
+  function _nextUnsafeSeed() private returns (bytes32) {
+    _s = keccak256(abi.encodePacked(blockhash(block.number - 1), block.coinbase, _s));
+    return _s;
   }
 }
