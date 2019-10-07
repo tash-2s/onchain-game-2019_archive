053b6e654c158e14c9e33ab471553a19a72b7055
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-10-07 22:34:55 +0900

    Use json data
    ---
    
    Original commit (web-client): bb18f9c6a88ad00363305ffc35db0fdd0fb3f1c9

--- web-client/src/data/NormalPlanets.ts
+++ web-client/src/data/NormalPlanets.ts
@@ -2,58 +2,24 @@ import BN from "bn.js"
 
 import { PlanetKind } from "../constants"
 
+import RawNormalPlanets from "./NormalPlanets.json"
+
 interface NormalPlanet {
   id: number
   kind: PlanetKind
   paramCommonLogarithm: number
   priceGoldCommonLogarithm: number
-  artSeedStr: string
+  param: BN
+  priceGold: BN
+  artSeed: BN
 }
 
-let id = 1
-let param = 1
-let planets: NormalPlanet[] = []
-while (id <= 16) {
-  planets.push({
-    id: id++,
-    kind: "residence",
-    paramCommonLogarithm: param,
-    priceGoldCommonLogarithm: param * 3,
-    artSeedStr: `${id - 1}`
-  })
-  param += 1
-  planets.push({
-    id: id++,
-    kind: "goldmine",
-    paramCommonLogarithm: param,
-    priceGoldCommonLogarithm: param * 3,
-    artSeedStr: `${id - 1}`
-  })
-  param += 1
-}
-
-planets = planets.concat([
-  {
-    id: 101,
-    kind: "technology",
-    paramCommonLogarithm: 2,
-    priceGoldCommonLogarithm: 8,
-    artSeedStr: "101"
-  },
-  {
-    id: 102,
-    kind: "technology",
-    paramCommonLogarithm: 3,
-    priceGoldCommonLogarithm: 12,
-    artSeedStr: "102"
-  }
-])
-
-export const NormalPlanets = planets.map(p => ({
+export const NormalPlanets: Array<NormalPlanet> = RawNormalPlanets.map(p => ({
   ...p,
+  kind: p.kind as PlanetKind,
   param: new BN(10).pow(new BN(p.paramCommonLogarithm)),
   priceGold: new BN(10).pow(new BN(p.priceGoldCommonLogarithm)),
-  artSeed: new BN(p.artSeedStr, 10)
+  artSeed: new BN(`${p.id - 1}`, 10)
 }))
 
 export const initialNormalPlanetIds = [1, 2]
