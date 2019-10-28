e7e61b99cba4bc5c09643878bb383bcec0a120e9
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-10-28 16:12:37 +0900

    Use functional component
    ---
    
    Original commit (web-client): 9191ccf444687c78fbe49b67c3819f88070ddc1b

--- web-client/src/components/UserPage/UserPlanetMap.tsx
+++ web-client/src/components/UserPage/UserPlanetMap.tsx
@@ -22,146 +22,130 @@ interface Props {
   isMine: boolean
 }
 
-interface State {
-  width: number | null
-}
-
 const placeholderId = "user-planet-map-placeholder"
 
-export class UserPlanetMap extends React.Component<Props, State> {
-  state: State = { width: null }
+export function UserPlanetMap(props: Props) {
+  const [width, setWidth] = React.useState<number | null>(null)
 
-  componentDidMount = () => {
-    if (!this.state.width) {
-      const e = document.getElementById(placeholderId)
-      if (e) {
-        this.setState({ width: e.clientWidth })
-      }
+  React.useEffect(() => {
+    const e = document.getElementById(placeholderId)
+    if (!e) {
+      return
     }
+    setWidth(e.clientWidth)
+  }, [])
+
+  if (!width) {
+    return <div id={placeholderId} />
   }
 
-  render = () => {
-    if (!this.state.width) {
-      return <div id={placeholderId} />
-    }
+  const shownRadius = props.user.map.shownRadius
+  // Show biggest map for this area
+  // Calc hex sizes to fit mapWidth
+  // mapWidth (= state.width) = (1.5 * hexSize) * (mapRadius * 2 + 1) + (0.5 * hexSize)
+  // Transform this equation for hexSize...
+  const hexSize = Math.min(width / (3 * shownRadius + 2), 100) // max: 100
+  const hexWidth = hexSize * 2
+  const hexHeight = Math.sqrt(3) * hexSize
 
-    const shownRadius = this.props.user.map.shownRadius
-    // Show biggest map for this area
-    // Calc hex sizes to fit mapWidth
-    // mapWidth (= state.width) = (1.5 * hexSize) * (mapRadius * 2 + 1) + (0.5 * hexSize)
-    // Transform this equation for hexSize...
-    const hexSize = Math.min(this.state.width / (3 * shownRadius + 2), 100) // max: 100
-    const hexWidth = hexSize * 2
-    const hexHeight = Math.sqrt(3) * hexSize
+  let isSufficientGoldForNext = true
+  const planetId = props.userPageUi.selectedNormalPlanetIdForSet
+  if (!!planetId && props.userPageUi.selectedPlanetHexesForSet.length > 0) {
+    const planet = getNormalPlanet(planetId)
+    const buyableCount = props.user.gold.div(planet.priceGold)
+    isSufficientGoldForNext = buyableCount.gtn(props.userPageUi.selectedPlanetHexesForSet.length)
+  }
 
-    let isSufficientGoldForNext = true
-    const planetId = this.props.userPageUi.selectedNormalPlanetIdForSet
-    if (!!planetId && this.props.userPageUi.selectedPlanetHexesForSet.length > 0) {
-      const planet = getNormalPlanet(planetId)
-      const buyableCount = this.props.user.gold.div(planet.priceGold)
-      isSufficientGoldForNext = buyableCount.gtn(
-        this.props.userPageUi.selectedPlanetHexesForSet.length
-      )
-    }
+  const hexes = props.user.map.hexes.map(h => {
+    const settable =
+      props.isMine &&
+      h.settable &&
+      (!!props.userPageUi.selectedNormalPlanetIdForSet ||
+        !!props.userPageUi.selectedSpecialPlanetTokenIdForSet) &&
+      isSufficientGoldForNext
 
-    const hexes = this.props.user.map.hexes.map(h => {
-      const settable =
-        this.props.isMine &&
-        h.settable &&
-        (!!this.props.userPageUi.selectedNormalPlanetIdForSet ||
-          !!this.props.userPageUi.selectedSpecialPlanetTokenIdForSet) &&
-        isSufficientGoldForNext
+    const isSelected = !!props.userPageUi.selectedPlanetHexesForSet.find(
+      o => o.axialCoordinateQ === h.q && o.axialCoordinateR === h.r
+    )
 
-      const isSelected = !!this.props.userPageUi.selectedPlanetHexesForSet.find(
-        o => o.axialCoordinateQ === h.q && o.axialCoordinateR === h.r
-      )
-
-      let selectFn: (() => void) | undefined = undefined
-      if (
-        !!this.props.userPageUi.selectedNormalPlanetIdForSet ||
-        !!this.props.userPageUi.selectedSpecialPlanetTokenIdForSet
-      ) {
-        if (settable) {
-          selectFn = this.selectPlanet(h.q, h.r)
-        }
-      } else if (!!h.userPlanet) {
-        if (h.userPlanet.isNormal) {
-          selectFn = () => {
-            if (!h.userPlanet) {
-              return
-            }
-            this.props.userPageUiActions.selectUserNormalPlanetForModal(h.userPlanet.id)
+    let selectFn: (() => void) | undefined = undefined
+    if (
+      !!props.userPageUi.selectedNormalPlanetIdForSet ||
+      !!props.userPageUi.selectedSpecialPlanetTokenIdForSet
+    ) {
+      if (settable) {
+        selectFn = () => {
+          if (props.userPageUi.selectedNormalPlanetIdForSet) {
+            props.userPageUiActions.selectPlanetHexForSet(h.q, h.r)
+            return
           }
-        } else {
-          selectFn = () => {
-            if (!h.userPlanet) {
-              return
-            }
-            this.props.userPageUiActions.selectUserSpecialPlanetForModal(h.userPlanet.id)
+          if (props.userPageUi.selectedSpecialPlanetTokenIdForSet) {
+            props.userActions.special.setPlanetTokenToMap(
+              props.userPageUi.selectedSpecialPlanetTokenIdForSet,
+              h.q,
+              h.r
+            )
+            props.userPageUiActions.unselectSpecialPlanetTokenForSet()
+            return
           }
+
+          throw new Error("this must be called with target")
         }
       }
-
-      return (
-        <PlanetHex
-          key={`${h.q}/${h.r}`}
-          q={h.q}
-          r={h.r}
-          userPlanet={h.userPlanet}
-          shiftTop={shownRadius * hexHeight}
-          shiftLeft={shownRadius * ((hexWidth / 4) * 3)}
-          hexSize={hexSize}
-          hexWidth={hexWidth}
-          hexHeight={hexHeight}
-          isSelected={isSelected}
-          select={selectFn}
-        />
-      )
-    })
-
-    const height = (shownRadius * 2 + 1) * hexHeight
-
-    let btn = <></>
-    if (!!planetId && this.props.userPageUi.selectedPlanetHexesForSet.length > 0) {
-      const fn = () => {
-        this.props.userActions.normal.setPlanetsToMap(
-          planetId,
-          this.props.userPageUi.selectedPlanetHexesForSet
-        )
-        this.props.userPageUiActions.unselectNormalPlanetForSet()
-        this.props.userPageUiActions.unselectPlanetHexesForSet()
+    } else if (!!h.userPlanet) {
+      if (h.userPlanet.isNormal) {
+        selectFn = () => {
+          if (!h.userPlanet) {
+            return
+          }
+          props.userPageUiActions.selectUserNormalPlanetForModal(h.userPlanet.id)
+        }
+      } else {
+        selectFn = () => {
+          if (!h.userPlanet) {
+            return
+          }
+          props.userPageUiActions.selectUserSpecialPlanetForModal(h.userPlanet.id)
+        }
       }
-      btn = <button onClick={fn}>set to map</button>
     }
 
     return (
-      <>
-        <WrappedModal {...this.props} />
-        {btn}
-        <div style={{ position: "relative", height: height }}>{hexes}</div>
-      </>
+      <PlanetHex
+        key={`${h.q}/${h.r}`}
+        q={h.q}
+        r={h.r}
+        userPlanet={h.userPlanet}
+        shiftTop={shownRadius * hexHeight}
+        shiftLeft={shownRadius * ((hexWidth / 4) * 3)}
+        hexSize={hexSize}
+        hexWidth={hexWidth}
+        hexHeight={hexHeight}
+        isSelected={isSelected}
+        select={selectFn}
+      />
     )
-  }
+  })
 
-  selectPlanet = (q: number, r: number) => {
-    return () => {
-      if (this.props.userPageUi.selectedNormalPlanetIdForSet) {
-        this.props.userPageUiActions.selectPlanetHexForSet(q, r)
-        return
-      }
-      if (this.props.userPageUi.selectedSpecialPlanetTokenIdForSet) {
-        this.props.userActions.special.setPlanetTokenToMap(
-          this.props.userPageUi.selectedSpecialPlanetTokenIdForSet,
-          q,
-          r
-        )
-        this.props.userPageUiActions.unselectSpecialPlanetTokenForSet()
-        return
-      }
+  const height = (shownRadius * 2 + 1) * hexHeight
 
-      throw new Error("this must be called with target")
+  let btn = <></>
+  if (!!planetId && props.userPageUi.selectedPlanetHexesForSet.length > 0) {
+    const fn = () => {
+      props.userActions.normal.setPlanetsToMap(planetId, props.userPageUi.selectedPlanetHexesForSet)
+      props.userPageUiActions.unselectNormalPlanetForSet()
+      props.userPageUiActions.unselectPlanetHexesForSet()
     }
+    btn = <button onClick={fn}>set to map</button>
   }
+
+  return (
+    <>
+      <WrappedModal {...props} />
+      {btn}
+      <div style={{ position: "relative", height: height }}>{hexes}</div>
+    </>
+  )
 }
 
 function WrappedModal(props: Props) {
