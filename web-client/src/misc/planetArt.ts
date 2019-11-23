6c52b8edaf74a753912014455434487217770d91
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-11-23 15:05:21 +0900

    Remove unnecessary comment
    ---
    
    Original commit (web-client): fd91bc58c7c5aa9b57a366c98c285803882dbcdb

--- web-client/src/misc/planetArt.ts
+++ web-client/src/misc/planetArt.ts
@@ -25,7 +25,6 @@ export const draw = (
       planetKind,
       artRarity,
       new SeededRandomish(artSeed)
-      // ArtStringController.generate(planetKind, artVersion, artRarity, artSeed)
     )
   }
 
