60c88dab3edf3746760a68c7d875cf02fc2746c9
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-11-26 12:45:29 +0900

    Use bit-op style UserGold
    ---
    
    Original commit (chain-contracts): 92318088e5092cccb6c982283638d62f4a25e089

---
+++ chain-contracts/loom/contracts/permanences/abstracts/AddressToBytes32Permanence.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.5.11;
+
+import "@openzeppelin/contracts/access/roles/MinterRole.sol";
+
+contract AddressToBytes32Permanence is MinterRole {
+  mapping(address => bytes32) private _addressToBytes32;
+
+  function read(address addr) public view returns (bytes32) {
+    return _addressToBytes32[addr];
+  }
+
+  function update(address addr, bytes32 b32) public onlyMinter {
+    _addressToBytes32[addr] = b32;
+  }
+}
