c8b0b63a8c345c408d9a0f8e61bd9b5943a24a15
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-11-26 12:48:22 +0900

    Rename and make private
    ---
    
    Original commit (chain-contracts): a0da5f8c66746c841e26e3c15257d55e6034b3cf

--- chain-contracts/SpecialPlanetTokenIdInterpretable.sol
+++ chain-contracts/SpecialPlanetTokenIdInterpretable.sol
@@ -1,22 +1,22 @@
 pragma solidity 0.5.11;
 
 contract SpecialPlanetTokenIdInterpretable {
-  uint8 constant SHORT_ID_START_BIT = 0;
-  uint8 constant VERSION_START_BIT = 24;
-  uint8 constant KIND_START_BIT = 24 + 8;
-  uint8 constant PARAM_RATE_START_BIT = 24 + 8 + 8;
-  uint8 constant ART_SEED_START_BIT = 24 + 8 + 8 + 8;
+  uint8 private constant _SHORT_ID_SHIFT_COUNT = 0;
+  uint8 private constant _VERSION_SHIFT_COUNT = 24;
+  uint8 private constant _KIND_SHIFT_COUNT = 24 + 8;
+  uint8 private constant _PARAM_RATE_SHIFT_COUNT = 24 + 8 + 8;
+  uint8 private constant _ART_SEED_SHIFT_COUNT = 24 + 8 + 8 + 8;
 
   function interpretSpecialPlanetTokenIdToFields(uint256 tokenId)
     internal
     pure
     returns (uint24 shortId, uint8 version, uint8 kind, uint8 paramRate, uint64 artSeed)
   {
-    shortId = uint24(tokenId >> SHORT_ID_START_BIT);
-    version = uint8(tokenId >> VERSION_START_BIT);
-    kind = uint8(tokenId >> KIND_START_BIT);
-    paramRate = uint8(tokenId >> PARAM_RATE_START_BIT);
-    artSeed = uint64(tokenId >> ART_SEED_START_BIT);
+    shortId = uint24(tokenId >> _SHORT_ID_SHIFT_COUNT);
+    version = uint8(tokenId >> _VERSION_SHIFT_COUNT);
+    kind = uint8(tokenId >> _KIND_SHIFT_COUNT);
+    paramRate = uint8(tokenId >> _PARAM_RATE_SHIFT_COUNT);
+    artSeed = uint64(tokenId >> _ART_SEED_SHIFT_COUNT);
   }
 
   function interpretSpecialPlanetTokenFieldsToId(
@@ -27,10 +27,10 @@ contract SpecialPlanetTokenIdInterpretable {
     uint64 artSeed
   ) internal pure returns (uint256) {
     return
-      (uint256(shortId) << SHORT_ID_START_BIT) |
-        (uint256(version) << VERSION_START_BIT) |
-        (uint256(kind) << KIND_START_BIT) |
-        (uint256(paramRate) << PARAM_RATE_START_BIT) |
-        (uint256(artSeed) << ART_SEED_START_BIT);
+      (uint256(shortId) << _SHORT_ID_SHIFT_COUNT) |
+        (uint256(version) << _VERSION_SHIFT_COUNT) |
+        (uint256(kind) << _KIND_SHIFT_COUNT) |
+        (uint256(paramRate) << _PARAM_RATE_SHIFT_COUNT) |
+        (uint256(artSeed) << _ART_SEED_SHIFT_COUNT);
   }
 }
