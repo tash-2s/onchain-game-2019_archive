05602d7f4701a8c973e45833173e7319374049f3
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-11-29 13:04:32 +0900

    Fix bug, and use functional component
    How to reproduce the bug:
    
    1. Go to a user page
    2. Change the URL to another user page
    3. Error happens
    
    It occurred because of the `stringify` comparison.
    It compared "state" and "computed state", they are different.
    
    This change fixes the error.
    
    ---
    
    Original commit (web-client): 8f426cc24827302414c3410319823fd785fc657a

--- web-client/src/components/UserPage.tsx
+++ web-client/src/components/UserPage.tsx
@@ -5,66 +5,40 @@ import { TargetUser } from "./UserPage/TargetUser"
 import { Tokens } from "./UserPage/Tokens"
 import { initialUserPageUIState } from "../reducers/userPageUIReducer"
 
-export class UserPage extends React.Component<UserPageProps> {
-  render = () => {
-    if (
-      this.props.user.targetUser &&
-      this.props.user.targetUser.address === this.props.route.params[0]
-    ) {
-      switch (this.props.userPageUI.selectedPageViewKind) {
-        case "main":
-          return (
-            <TargetUser
-              currentUser={this.props.currentUser}
-              time={this.props.time}
-              userPageUI={this.props.userPageUI}
-              targetUser={this.props.user.targetUser}
-              userPageUIActions={this.props.userPageUIActions}
-              userActions={this.props.userActions}
-            />
-          )
-        case "tokens":
-          return (
-            <Tokens
-              user={this.props.user.targetUser}
-              userActionsForSpecialPlanet={this.props.userActions.special}
-              userPageUIActions={this.props.userPageUIActions}
-            />
-          )
-        default:
-          throw new Error("undefined view kind")
-      }
-    } else {
-      return <div>loading...</div>
+export function UserPage(props: UserPageProps) {
+  React.useEffect(() => {
+    props.userActions.main.setTargetUser(props.route.params[0])
+    return () => {
+      props.userActions.main.clearTargetUser()
+      props.userPageUIActions.clear()
     }
+  }, [props.route.params[0]])
+
+  if (!props.user.targetUser || props.user.targetUser.address !== props.route.params[0]) {
+    return <div>loading...</div>
   }
 
-  componentDidMount = () => {
-    this.props.userActions.main.setTargetUser(this.props.route.params[0])
+  switch (props.userPageUI.selectedPageViewKind) {
+    case "main":
+      return (
+        <TargetUser
+          currentUser={props.currentUser}
+          time={props.time}
+          userPageUI={props.userPageUI}
+          targetUser={props.user.targetUser}
+          userPageUIActions={props.userPageUIActions}
+          userActions={props.userActions}
+        />
+      )
+    case "tokens":
+      return (
+        <Tokens
+          user={props.user.targetUser}
+          userActionsForSpecialPlanet={props.userActions.special}
+          userPageUIActions={props.userPageUIActions}
+        />
+      )
+    default:
+      throw new Error("undefined view kind")
   }
-
-  componentDidUpdate(prevProps: UserPageProps) {
-    if (
-      this.props.user.targetUser &&
-      this.props.user.targetUser.address !== this.props.route.params[0]
-    ) {
-      if (stringify(this.props.userPageUI) !== stringify(initialUserPageUIState)) {
-        this.props.userPageUIActions.clear()
-        return
-      }
-      this.props.userActions.main.setTargetUser(this.props.route.params[0])
-    }
-  }
-
-  componentWillUnmount = () => {
-    this.props.userActions.main.clearTargetUser()
-    this.props.userPageUIActions.clear()
-  }
-}
-
-const stringify = (o: object) => {
-  return Object.entries(o)
-    .map(a => `${a[0]}:${a[1]}`)
-    .sort()
-    .join(",")
 }
