9e74012d8e42e4479415af62331fa9c4eaab047d
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-12-08 00:04:57 +0900

    Use pre-exist abstract permanence
    ---
    
    Original commit (chain-contracts): 45ac3119605498a5ce7e499040ab8c16daf7e361

--- chain-contracts/loom/contracts/permanences/SpecialPlanetIdToDataPermanence.sol
+++ chain-contracts/loom/contracts/permanences/SpecialPlanetIdToDataPermanence.sol
@@ -1,15 +1,5 @@
 pragma solidity 0.5.13;
 
-import "@openzeppelin/contracts/access/roles/WhitelistedRole.sol";
+import "./abstracts/Uint256ToBytes32Permanence.sol";
 
-contract SpecialPlanetIdToDataPermanence is WhitelistedRole {
-  mapping(uint24 => bytes32) private _map;
-
-  function read(uint24 key) public view returns (bytes32) {
-    return _map[key];
-  }
-
-  function update(uint24 key, bytes32 value) public onlyWhiteliste {
-    _map[key] = value;
-  }
-}
+contract SpecialPlanetIdToDataPermanence is Uint256ToBytes32Permanence {}
