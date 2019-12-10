20ad48d8d6a6bd1e965362f3cd29a1fcdce22fbc
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-12-10 18:18:15 +0900

    Add MigrationResult test
    ---
    
    Original commit (chain-contracts): 691da54d1c2c41c67a49ad0e7ff378d578b3dbf1

--- chain-contracts/eth/migrations/1565790363_createSpecialPlanetToken.js
+++ chain-contracts/eth/migrations/1565790363_createSpecialPlanetToken.js
@@ -2,20 +2,19 @@ const SpecialPlanetToken = artifacts.require("SpecialPlanetToken")
 const SpecialPlanetTokenShop = artifacts.require("SpecialPlanetTokenShop")
 const SpecialPlanetTokenShortIdGenerator = artifacts.require("SpecialPlanetTokenShortIdGenerator")
 
-module.exports = function(deployer, network, accounts) {
+module.exports = function(deployer, network, [account]) {
   deployer.then(async function() {
-    await deployer.deploy(SpecialPlanetToken, getGatewayAddress(network, accounts[0]))
-    await deployer.deploy(SpecialPlanetTokenShortIdGenerator)
-    await deployer.deploy(
+    const token = await deployer.deploy(SpecialPlanetToken, getGatewayAddress(network, account))
+    const idGenerator = await deployer.deploy(SpecialPlanetTokenShortIdGenerator)
+    const shop = await deployer.deploy(
       SpecialPlanetTokenShop,
       SpecialPlanetToken.address,
       SpecialPlanetTokenShortIdGenerator.address
     )
 
-    await (await SpecialPlanetToken.deployed()).addWhitelisted(SpecialPlanetTokenShop.address)
-    await (await SpecialPlanetTokenShortIdGenerator.deployed()).addWhitelisted(
-      SpecialPlanetTokenShop.address
-    )
+    await token.addWhitelisted(SpecialPlanetTokenShop.address)
+    await idGenerator.addWhitelisted(SpecialPlanetTokenShop.address)
+    await shop.addWhitelisted(account)
   })
 }
 
---
+++ chain-contracts/eth/test/TestMigrationResult.js
@@ -0,0 +1,67 @@
+const SpecialPlanetToken = artifacts.require("SpecialPlanetToken")
+const SpecialPlanetTokenShop = artifacts.require("SpecialPlanetTokenShop")
+const SpecialPlanetTokenShortIdGenerator = artifacts.require("SpecialPlanetTokenShortIdGenerator")
+
+contract("Migration Result", function([deployer, stranger]) {
+  before(async function() {
+    this.token = await SpecialPlanetToken.deployed()
+    this.shop = await SpecialPlanetTokenShop.deployed()
+    this.idGenerator = await SpecialPlanetTokenShortIdGenerator.deployed()
+  })
+
+  it("should have valid whitelisted accounts", async function() {
+    assert.deepEqual(await getWhitelistedAccounts(this.token), [this.shop.address])
+    assert.deepEqual(await getWhitelistedAccounts(this.shop), [deployer])
+    assert.deepEqual(await getWhitelistedAccounts(this.idGenerator), [this.shop.address])
+  })
+
+  it("should have valid whitelistAdmin accounts", async function() {
+    assert.deepEqual(await getWhitelistAdminAccounts(this.token), [deployer])
+    assert.deepEqual(await getWhitelistAdminAccounts(this.shop), [deployer])
+    assert.deepEqual(await getWhitelistAdminAccounts(this.idGenerator), [deployer])
+  })
+})
+
+const whitelistEventABI = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "account",
+        type: "address"
+      }
+    ],
+    name: "WhitelistAdminAdded",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "account",
+        type: "address"
+      }
+    ],
+    name: "WhitelistedAdded",
+    type: "event"
+  }
+]
+
+const getWhitelistedAccounts = async truffleContract => {
+  return await _getWhitelistAccounts(truffleContract, "WhitelistedAdded")
+}
+
+const getWhitelistAdminAccounts = async truffleContract => {
+  return await _getWhitelistAccounts(truffleContract, "WhitelistAdminAdded")
+}
+
+const _getWhitelistAccounts = async (truffleContract, eventName) => {
+  const web3Contract = new web3.eth.Contract(whitelistEventABI, truffleContract.address)
+  return (await web3Contract.getPastEvents(eventName, {fromBlock: 0, toBlock: "latest"})).map(
+    e => e.returnValues.account
+  )
+}
