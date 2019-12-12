8805a8ac300fd0b8d7e0e3b6562cc8d73a6d4927
tash-2s <81064017+tash-2s@users.noreply.github.com>
2019-12-12 15:18:30 +0900

    Fix TestSpecialPlanetControllable
    ---
    
    Original commit (chain-contracts): fdfd14cba0f3a080f72da4f61993fa62e5b0b8c8

--- chain-contracts/loom/test/controllers/modules/TestSpecialPlanetControllable.sol
+++ chain-contracts/loom/test/controllers/modules/TestSpecialPlanetControllable.sol
@@ -3,47 +3,48 @@ pragma solidity 0.5.13;
 import "truffle/Assert.sol";
 
 import "../../../contracts/controllers/modules/SpecialPlanetControllable.sol";
-import "../../../contracts/permanences/UserSpecialPlanetPermanence.sol";
-import "../../../contracts/permanences/SpecialPlanetIdToDataPermanence.sol";
 
 contract TestSpecialPlanetControllable is SpecialPlanetControllable {
-  UserSpecialPlanetPermanence private _p = new UserSpecialPlanetPermanence();
-  SpecialPlanetIdToDataPermanence private _idToDataP = new SpecialPlanetIdToDataPermanence();
-
-  function beforeAll() public {
-    setUserSpecialPlanetPermanence(address(_p));
-    setSpecialPlanetIdToDataPermanence(address(_idToDataP));
+  function beforeEach() public {
+    userSpecialPlanetPermanence = new UserSpecialPlanetPermanence();
+    specialPlanetIdToDataPermanence = new SpecialPlanetIdToDataPermanence();
   }
 
   function testBuildUserSpecialPlanetRecordFromBytes32() public {
     _assertEqual(
       buildUserSpecialPlanetRecordFromBytes32(
-        bytes32(0x0000000000000000000000000000000800070000000600000005040302000001)
+        0x0000000000000000000000000a00090008000000070000000605040302000001
       ),
-      UserSpecialPlanetRecord(1, 2, 3, 4, 5, 6, 7, 8),
-      "1 to 8"
-    );
-    _assertEqual(
-      buildUserSpecialPlanetRecordFromBytes32(
-        bytes32(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
-      ),
-      UserSpecialPlanetRecord(16777215, 255, 255, 255, 4294967295, 4294967295, -1, -1),
-      "all f"
+      UserSpecialPlanetRecord(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+      "1 to 10"
     );
   }
 
   function testBuildBytes32FromUserSpecialPlanetRecord() public {
     Assert.equal(
-      buildBytes32FromUserSpecialPlanetRecord(UserSpecialPlanetRecord(1, 2, 3, 4, 5, 6, 7, 8)),
-      bytes32(0x0000000000000000000000000000000800070000000600000005040302000001),
-      "1 to 8"
+      buildBytes32FromUserSpecialPlanetRecord(
+        UserSpecialPlanetRecord(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+      ),
+      0x0000000000000000000000000a00090008000000070000000605040302000001,
+      "1 to 10"
     );
     Assert.equal(
       buildBytes32FromUserSpecialPlanetRecord(
-        UserSpecialPlanetRecord(16777215, 255, 255, 255, 4294967295, 4294967295, -1, -1)
+        UserSpecialPlanetRecord(
+          16777215,
+          255,
+          255,
+          255,
+          255,
+          4294967295,
+          4294967295,
+          -1,
+          -1,
+          18446744073709551615
+        )
       ),
-      bytes32(0x0000000000000000000000000000ffffffffffffffffffffffffffffffffffff),
-      "all f"
+      0x0000000000ffffffffffffffffffffffffffffffffffffffffffffffffffffff,
+      "max"
     );
   }
 
@@ -56,25 +57,29 @@ contract TestSpecialPlanetControllable is SpecialPlanetControllable {
       keccak256(
         abi.encodePacked(
           r1.id,
+          r1.version,
           r1.kind,
-          r1.originalParamCommonLogarithm,
+          r1.paramRate,
           r1.rank,
           r1.rankupedAt,
           r1.createdAt,
           r1.coordinateQ,
-          r1.coordinateR
+          r1.coordinateR,
+          r1.artSeed
         )
       ),
       keccak256(
         abi.encodePacked(
           r2.id,
+          r2.version,
           r2.kind,
-          r2.originalParamCommonLogarithm,
+          r2.paramRate,
           r2.rank,
           r2.rankupedAt,
           r2.createdAt,
           r2.coordinateQ,
-          r2.coordinateR
+          r2.coordinateR,
+          r2.artSeed
         )
       ),
       message
